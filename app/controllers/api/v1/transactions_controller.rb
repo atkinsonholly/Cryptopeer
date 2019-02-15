031581622e72f5971fcd4efcbc04b60e1d2f5b1d
Lingy94 <j.lingyx@gmail.com>
2019-02-15 12:59:40 +0000

    Updated transactions controller and sorting for user_coins

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -11,12 +11,48 @@ class Api::V1::TransactionsController < ApplicationController
   end
 
   def create
-    @transaction = Transaction.create!(user_one_id: params[:user_one_id], user_two_id: params[:user_two_id], user_one_amount: params[:user_one_amount], user_two_amount: params[:user_two_amount], user_one_currency_symbol: params[:user_one_currency_symbol], user_two_currency_symbol: params[:user_two_currency_symbol])
+    
+    # byebug
+    user1CurrencySymbol = Coin.find(params[:trading_state][:user1CurrencyId]).symbol
+    user2CurrencySymbol = Coin.find(params[:trading_state][:user2CurrencyId]).symbol
+    @transaction = Transaction.create!(user_one_id: params[:trading_state][:user1Id],
+                                      user_two_id: params[:trading_state][:user2Id],
+                                      user_one_amount: params[:trading_state][:user1Amount],
+                                      user_two_amount: params[:trading_state][:user2Amount],
+                                      user_one_currency_symbol: user1CurrencySymbol,
+                                      user_two_currency_symbol: user2CurrencySymbol,
+                                      user_one_price: params[:trading_state][:user1Price],
+                                      user_two_price: params[:trading_state][:user2Price])
+    puts @transaction
     if @transaction.valid?
-      render json: @transaction
+      @user1 = User.find(params[:trading_state][:user1Id])
+      @user2 = User.find(params[:trading_state][:user2Id])
+
+      @user1coindecrease = @user1.user_coins.find{|coin| coin.coin_id == params[:trading_state][:user1CurrencyId].to_i}
+      @user1coindecreasequantity = @user1coindecrease.quantity - params[:trading_state][:user1Amount].to_f
+      @user1coindecrease.update(quantity: @user1coindecreasequantity)
+      
+      @user1coinincrease = @user1.user_coins.find{|coin| coin.coin_id == params[:trading_state][:user2CurrencyId].to_i}
+      @user1coinincreasequantity = @user1coinincrease.quantity + params[:trading_state][:user2Amount].to_f
+      @user1coinincrease.update(quantity: @user1coinincreasequantity)
+      
+      
+      @user2coindecrease = @user2.user_coins.find{|coin| coin.coin_id == params[:trading_state][:user2CurrencyId].to_i}
+      @user2coindecreasequantity = @user2coindecrease.quantity - params[:trading_state][:user2Amount].to_f
+      @user2coindecrease.update(quantity: @user2coindecreasequantity)
+      
+      @user2coinincrease = @user2.user_coins.find{|coin| coin.coin_id == params[:trading_state][:user1CurrencyId].to_i}
+      @user2coinincreasequantity = @user2coinincrease.quantity + params[:trading_state][:user1Amount].to_f
+      @user2coinincrease.update(quantity: @user2coinincreasequantity)
+      @users = User.all
+      render json: @users
     else
       render json: {error: "Unable to create transaction"}, status: 400
     end
   end
+
+  # def trading_params
+    # params.require(:trading_state).
+  # end
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -5,10 +5,11 @@ class UserSerializer < ActiveModel::Serializer
   # has_many :coins, through: :user_coins
 
   def user_coins
-    UserCoin.where(user_id: self.object.id).map{ |usercoin| {coin_id: usercoin.coin_id, id: usercoin.id, quantity: usercoin.quantity, selling: usercoin.selling, user_id: usercoin.user_id, symbol: usercoin.coin.symbol, name: usercoin.coin.name, price: usercoin.coin.price}}
+    UserCoin.where(user_id: self.object.id).map{ |usercoin| {coin_id: usercoin.coin_id, id: usercoin.id, quantity: usercoin.quantity, selling: usercoin.selling, user_id: usercoin.user_id, symbol: usercoin.coin.symbol, name: usercoin.coin.name, price: usercoin.coin.price}}.sort_by{|i| i[:coin_id]}
+
   end
 
   def transactions
-    Transaction.where("user_one_id = ? OR user_two_id = ?", self.object, self.object)
+    Transaction.where("user_one_id = ? OR user_two_id = ?", self.object, self.object).sort
   end
 end
